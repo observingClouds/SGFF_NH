9eb38224c5d737ea40cd214ace8261608f709d78
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2021-04-14 18:58:47 +0200

    + Postprocessing of 1x1 data from zarr to nc

--- analysis/run_prepare_data_grid.sh
+++ analysis/run_prepare_data_grid.sh
@@ -16,3 +16,7 @@ git --describe --always
 cd /work/mh0010/m300408/CloudMorphology/analysis/
 python prepare_data_grid.py
 
+source activate /work/mh0010/m300408/anaconda3/envs/BCOenv
+python convert_data_grid.py
+mv ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc2 ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc
+ 
