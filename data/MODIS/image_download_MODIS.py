7b7f707d098f07d2b7ffddcba92d4fafbabfb55c
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-10-14 22:45:57 +0200

    add scripts to download MODIS data

---
+++ data/MODIS/image_download_MODIS.py
@@ -0,0 +1,48 @@
+"""
+Script to download images from NASA Worldview
+"""
+import urllib.request
+import datetime
+from calendar import monthrange
+import fire
+import os
+
+
+def download_MODIS_imgs(year_range, months, save_path, lon_range, lat_range,
+                  deg2pix=100, satellite='Aqua', exist_skip=False, var='CorrectedReflectance_TrueColor',filetype='jpeg'):
+    os.makedirs(save_path, exist_ok=True)
+
+    lon1 = lon_range[0]; lon2 = lon_range[1]
+    lat1 = lat_range[0]; lat2 = lat_range[1]
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    loc= (f'&BBOX={lat1},{lon1},{lat2},{lon2}')
+    loc_str = f'_{lon1}-{lon2}_{lat1}-{lat2}'
+    size = (f'&WIDTH={int(dlon * deg2pix)}&HEIGHT={int(dlat * deg2pix)}')
+    layer = (f'&LAYERS=MODIS_{satellite}_{var},Coastlines')
+
+    for yr in range(year_range[0], year_range[1]):
+        for m in months:
+            nday = monthrange(yr,m)[1]
+            for nd in range(1,nday+1):
+                date = datetime.datetime(yr, m, nd)
+                d = str(date.strftime('%j'))
+                print(date.strftime('%y %m %d'))
+                url = ('https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME='+
+                       date.strftime('%Y-%m-%d')+loc+'&CRS=EPSG:4326'+
+                       layer+'&FORMAT=image/'+filetype+
+                       size)
+                save_str = (save_path+f'/{satellite}_'+var+str(yr)+
+                    date.strftime('%m')+'{:02d}'.format(date.day)+loc_str+
+                    '.'+filetype)
+                if exist_skip and os.path.exists(save_str):
+                    print('Skip')
+                else:
+                    try:
+                        urllib.request.urlretrieve(url, save_str)
+                    except:
+                        print(f'Download failed for {save_str} ({url})')
+
+
+if __name__ == '__main__':
+    fire.Fire(download_MODIS_imgs)
---
+++ data/MODIS/run_download_MODIS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J worldview          # Specify job name
+#SBATCH -p shared             # Use partition prepost
+#SBATCH -t 12:00:00             # Set a limit on the total run time
+#SBATCH --chdir /work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/data/MODIS
+#SBATCH -A mh0010              # Charge resources on this project account
+#SBATCH -o worldview.o%j          # File name for standard output
+#SBATCH -e worldview.e%j          # File name for standard error output
+
+mamba activate covariability
+
+# # INFRARED IMAGES
+output_dir=Aqua_MODIS_IR
+mkdir -p $output_dir
+
+python image_download_MODIS.py --year_range [2007,2010] --months [1,2,3,4,5,6,7,8,9,10,11,12] --lon_range [-100,10] --lat_range [-10,55] --save_path $output_dir --satellite Aqua --var Brightness_Temp_Band31_Day
