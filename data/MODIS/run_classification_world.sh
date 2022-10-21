f9ea00bbf3932c058329a275159ed827bd72917e
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-10-21 06:43:31 +0200

    increase resources (previous setup 15h for 500 images)

--- data/MODIS/run_classification_world.sh
+++ data/MODIS/run_classification_world.sh
@@ -2,17 +2,17 @@
 #SBATCH --account=mh0010
 #SBATCH --job-name=classify_world.run
 #SBATCH --partition=gpu
-#SBATCH --chdir=/work/mh0010/m300408/CharacterizationOfMesoscalePatterns/Classification/NN-application
+#SBATCH --chdir=/work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/data/MODIS
 #SBATCH --nodes=1
-#SBATCH --output=/work/mh0010/m300408/CharacterizationOfMesoscalePatterns/Classification/NN-application/logs/LOG.run_classification_world.%j.o
-#SBATCH --error=/work/mh0010/m300408/CharacterizationOfMesoscalePatterns/Classification/NN-application/logs/LOG.run_classification_world.%j.o
+#SBATCH --output=/work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/data/MODIS/logs/LOG.run_classification_world.%j.o
+#SBATCH --error=/work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/data/MODIS/logs/LOG.run_classification_world.%j.o
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=248000
 #SBATCH --time=12:00:00
 #SBATCH --mail-user=hauke.schulz@mpimet.mpg.de
 #SBATCH --mail-type=ALL
 
-module load anaconda3
-source activate /work/mh0010/m300408/anaconda3/envs/github_sugar
+source activate /home/m/m300408/.conda/envs/sgff
 
 python classification_world.py
+
