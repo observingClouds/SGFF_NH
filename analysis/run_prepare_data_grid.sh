1320e0fdd1647a2207a591d24c807b7be8641925
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-10-22 08:48:37 +0200

    update run_prepare_data_grid.sh (paths; environment)

--- analysis/run_prepare_data_grid.sh
+++ analysis/run_prepare_data_grid.sh
@@ -2,20 +2,17 @@
 #-----------------------------------------------------------------------------
 #SBATCH --account=mh0010
 #SBATCH --job-name=prepare_data_grid
-#SBATCH --partition=prepost
+#SBATCH --partition=compute
 #SBATCH --mem-per-cpu=244000     # Specify real memory required per CPU in MegaBytes
-#SBATCH --output=/work/mh0010/m300408/CloudMorphology/analysis/logs/LOG.prepare_data_grid.%j.o
-#SBATCH --error=/work/mh0010/m300408/CloudMorphology/analysis/logs/LOG.prepare_data_grid.%j.o
+#SBATCH --output=/work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/analysis/logs/LOG.prepare_data_grid.%j.o
+#SBATCH --error=/work/mh0010/from_Mistral/mh0010/m300408/CloudMorphology/analysis/logs/LOG.prepare_data_grid.%j.o
 #SBATCH --exclusive
-#
-#SBATCH --time=12:00:00
+#SBATCH --time=04:00:00
 #=============================================================================
-module load anaconda3
-source activate /work/mh0010/m300408/anaconda3/envs/radiation_project
+module load git
+source activate sgff_post
 git --describe --always
-cd /work/mh0010/m300408/CloudMorphology/analysis/
 python prepare_data_grid.py
 
-source activate /work/mh0010/m300408/anaconda3/envs/BCOenv
-python convert_data_grid.py
-mv ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc2 ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc
+#python convert_data_grid.py
+#mv ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc2 ../data/Daily_1x1_MODIS-IR_NorthAtlantic_SGFF.nc
