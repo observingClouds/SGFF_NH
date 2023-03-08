992310e729a38b5399e07c5ebdad37393d339630
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2023-03-08 06:41:08 +0000

    run download images in parallel

--- data/MODIS/image_download_MODIS.py
+++ data/MODIS/image_download_MODIS.py
@@ -1,6 +1,8 @@
 """
 Script to download images from NASA Worldview
 """
+import multiprocessing
+from functools import partial
 import datetime
 import os
 import urllib.request
@@ -9,8 +11,7 @@ from calendar import monthrange
 import fire
 from omegaconf import OmegaConf
 
-
-def download_MODIS_imgs(
+def get_setup(
     experiment=None,
     year_range=[],
     months=[],
@@ -47,44 +48,64 @@ def download_MODIS_imgs(
     loc_str = f"_{lon1}-{lon2}_{lat1}-{lat2}"
     size = f"&WIDTH={int(dlon * deg2pix)}&HEIGHT={int(dlat * deg2pix)}"
     layer = f"&LAYERS=MODIS_{satellite}_{var}"  # ,Coastlines"
+    cfg = {'year_range':year_range, 'months':months}
 
     os.makedirs(save_path, exist_ok=True)
 
-    for yr in range(year_range[0], year_range[1]):
-        for m in months:
-            nday = monthrange(yr, m)[1]
-            for nd in range(1, nday + 1):
-                date = datetime.datetime(yr, m, nd)
-                print(date.strftime("%y %m %d"))
-                url = (
+    url_fmt = (
                     "https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&TIME="
-                    + date.strftime("%Y-%m-%d")
+                    + "%Y-%m-%d"  # date.strftime("%Y-%m-%d")
                     + loc
                     + "&CRS=EPSG:4326"
                     + layer
                     + "&FORMAT=image/"
                     + filetype
-                    + size
-                )
-                save_str = (
+                    + size )
+    output_fmt = (
                     save_path
                     + f"/{satellite}_"
                     + var
-                    + str(yr)
-                    + date.strftime("%m")
-                    + f"{date.day:02d}"
+                    + "%Y%m%d"  # str(yr)
+                    #+ date.strftime("%m")
+                    #+ f"{date.day:02d}"
                     + loc_str
                     + "."
                     + filetype
                 )
-                if exist_skip and os.path.exists(save_str):
-                    print("Skip")
-                else:
-                    try:
-                        urllib.request.urlretrieve(url, save_str)
-                    except:
-                        print(f"Download failed for {save_str} ({url})")
+    return url_fmt, output_fmt, cfg
+
+def download_MODIS_img(url, output, exist_skip=False):
+    if exist_skip and os.path.exists(output):
+        print("Skip")
+    else:
+        try:
+            urllib.request.urlretrieve(url, output)
+        except:
+            print(f"Download failed for {output} ({url})")
+
+
+def get_MODIS_img(date, url_fmt, output_fmt):
+    print(date.strftime("%y %m %d"))
+    url = date.strftime(url_fmt)
+    output = date.strftime(output_fmt)
+    download_MODIS_img(url, output)
+
+
+def dates(year_range, months):
+    for yr in range(year_range[0], year_range[1]):
+        for m in months:
+            nday = monthrange(yr, m)[1]
+            for nd in range(1, nday + 1):
+                date = datetime.datetime(yr, m, nd)
+                yield date
 
 
 if __name__ == "__main__":
-    fire.Fire(download_MODIS_imgs)
+    pool = multiprocessing.Pool(8)
+
+    url_fmt, output_fmt, cfg = fire.Fire(get_setup)
+    year_range = cfg['year_range']
+    months = cfg['months']
+    f = partial(get_MODIS_img,url_fmt=url_fmt, output_fmt=output_fmt)
+    pool.map(f, dates(year_range, months))
+
