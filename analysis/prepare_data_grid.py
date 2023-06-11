7e3a8b12cbbed0a08e09dd5dfb5a4d2f638bbe88
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2023-06-11 03:06:45 +0000

    WIP: faster compute

--- analysis/prepare_data_grid.py
+++ analysis/prepare_data_grid.py
@@ -8,6 +8,7 @@ import numpy as np
 import tqdm
 import xarray as xr
 import zarr
+from dask.distributed import Client, LocalCluster
 
 print("Finished loading modules")
 
@@ -38,105 +39,105 @@ def calculate_mean(da):
     return lat_lon_binned
 
 
-ds_classifications_input = xr.open_zarr(fn_zarr)
+if __name__ == "__main__":
+    with dask.config.set({"distributed.scheduler.worker-saturation": 1}):
+        cluster = LocalCluster()
+        client = Client(cluster)
+    print(client)
 
+    ds_classifications_input = xr.open_zarr(fn_zarr, chunks={"time": 100})
 
-# Create file and calculate common boxes
-print("Create output file")
-nb_times = len(ds_classifications_input.time)
-nb_lons = len(lon_center)
-nb_lats = len(lat_center)
-nb_patterns = len(ds_classifications_input.pattern)
+    # Create file and calculate common boxes
+    print("Create output file")
+    nb_times = len(ds_classifications_input.time)
+    nb_lons = len(lon_center)
+    nb_lats = len(lat_center)
+    nb_patterns = len(ds_classifications_input.pattern)
 
-if os.path.exists(output_file) and not overwrite:
-    print("File already exists.")
-    root_grp = zarr.open(output_file)
-    count = root_grp["counts"]
-    times = root_grp["time"]
-    store = root_grp.store
-else:
-    print("File will be newly created.")
-    store = zarr.DirectoryStore(output_file)
-    root_grp = zarr.group(store, overwrite=True)
-    count = root_grp.create_dataset(
-        "counts",
-        shape=(nb_times, nb_lons, nb_lats, nb_patterns),
-        chunks=(1, nb_lons, nb_lats, nb_patterns),
-        dtype=float,
-        compressor=zarr.Zlib(level=1),
-    )
-    times = root_grp.create_dataset(
-        "time",
-        shape=(nb_times),
-        chunks=(nb_times),
-        dtype="<M8[ns]",
-        compressor=zarr.Zlib(level=1),
-    )
-    lats = root_grp.create_dataset(
-        "latitude",
-        shape=(nb_lats),
-        chunks=(nb_lats),
-        dtype=float,
-        compressor=zarr.Zlib(level=1),
-    )
-    lons = root_grp.create_dataset(
-        "longitude",
-        shape=(nb_lons),
-        chunks=(nb_lons),
-        dtype=float,
-        compressor=zarr.Zlib(level=1),
-    )
-    patterns = root_grp.create_dataset(
-        "class",
-        shape=(nb_patterns),
-        chunks=(nb_patterns),
-        dtype=str,
-        compressor=zarr.Zlib(level=1),
-    )
+    if os.path.exists(output_file) and not overwrite:
+        print("File already exists.")
+        root_grp = zarr.open(output_file)
+        count = root_grp["counts"]
+        times = root_grp["time"]
+        store = root_grp.store
+    else:
+        print("File will be newly created.")
+        store = zarr.DirectoryStore(output_file)
+        root_grp = zarr.group(store, overwrite=True)
+        count = root_grp.create_dataset(
+            "counts",
+            shape=(nb_times, nb_lons, nb_lats, nb_patterns),
+            chunks=(1, nb_lons, nb_lats, nb_patterns),
+            dtype=float,
+            compressor=zarr.Zlib(level=1),
+        )
+        times = root_grp.create_dataset(
+            "time",
+            shape=(nb_times),
+            chunks=(nb_times),
+            dtype="<M8[ns]",
+            compressor=zarr.Zlib(level=1),
+        )
+        lats = root_grp.create_dataset(
+            "latitude",
+            shape=(nb_lats),
+            chunks=(nb_lats),
+            dtype=float,
+            compressor=zarr.Zlib(level=1),
+        )
+        lons = root_grp.create_dataset(
+            "longitude",
+            shape=(nb_lons),
+            chunks=(nb_lons),
+            dtype=float,
+            compressor=zarr.Zlib(level=1),
+        )
+        patterns = root_grp.create_dataset(
+            "class",
+            shape=(nb_patterns),
+            chunks=(nb_patterns),
+            dtype=str,
+            compressor=zarr.Zlib(level=1),
+        )
 
-    lons[:] = lon_center
-    lats[:] = lat_center
-    patterns[:] = [label_map_rv[i] for i in range(4)]
+        lons[:] = lon_center
+        lats[:] = lat_center
+        patterns[:] = [label_map_rv[i] for i in range(4)]
 
-    # Add attributes to file
-    # Variable attributes
-    count.attrs["_ARRAY_DIMENSIONS"] = ("time", "longitude", "latitude", "class")
-    count.attrs[
-        "description"
-    ] = "number of classifications of particular pattern within 2.5 deg x 2.5 deg"
-    lons.attrs["_ARRAY_DIMENSIONS"] = "longitude"
-    lons.attrs["standard_name"] = "longitude"
-    lons.attrs["units"] = "degree_east"
-    lats.attrs["_ARRAY_DIMENSIONS"] = "latitude"
-    lats.attrs["standard_name"] = "latitude"
-    lats.attrs["units"] = "degree_north"
-    times.attrs["_ARRAY_DIMENSIONS"] = "time"
-    times.attrs[
-        "description"
-    ] = "classification id (basically each sighting of an image has a unique id)"
-    patterns.attrs["_ARRAY_DIMENSIONS"] = "class"
+        # Add attributes to file
+        # Variable attributes
+        count.attrs["_ARRAY_DIMENSIONS"] = ("time", "longitude", "latitude", "class")
+        count.attrs[
+            "description"
+        ] = "number of classifications of particular pattern within 2.5 deg x 2.5 deg"
+        lons.attrs["_ARRAY_DIMENSIONS"] = "longitude"
+        lons.attrs["standard_name"] = "longitude"
+        lons.attrs["units"] = "degree_east"
+        lats.attrs["_ARRAY_DIMENSIONS"] = "latitude"
+        lats.attrs["standard_name"] = "latitude"
+        lats.attrs["units"] = "degree_north"
+        times.attrs["_ARRAY_DIMENSIONS"] = "time"
+        times.attrs[
+            "description"
+        ] = "classification id (basically each sighting of an image has a unique id)"
+        patterns.attrs["_ARRAY_DIMENSIONS"] = "class"
 
-    # Global attributes
-    root_grp.attrs[
-        "title"
-    ] = "IR neural network meso-scale cloud pattern classifications (TropicalBelt)"
-    root_grp.attrs["description"] = "NN detections of meso-scale cloud patterns"
-    root_grp.attrs["author"] = "Hauke Schulz (haschulz@uw.edu)"
-    root_grp.attrs["institute"] = "University of Washington, USA"
-    root_grp.attrs["created_on"] = dt.datetime.now().strftime("%Y-%m-%d %H:%M UTC")
-    root_grp.attrs[
-        "history"
-    ] = f"grid averaging by prepare_data_grid of original file ({fn_zarr})"
-    root_grp.attrs["python_version"] = f"{sys.version}"
+        # Global attributes
+        root_grp.attrs[
+            "title"
+        ] = "IR neural network meso-scale cloud pattern classifications (TropicalBelt)"
+        root_grp.attrs["description"] = "NN detections of meso-scale cloud patterns"
+        root_grp.attrs["author"] = "Hauke Schulz (haschulz@uw.edu)"
+        root_grp.attrs["institute"] = "University of Washington, USA"
+        root_grp.attrs["created_on"] = dt.datetime.now().strftime("%Y-%m-%d %H:%M UTC")
+        root_grp.attrs[
+            "history"
+        ] = f"grid averaging by prepare_data_grid of original file ({fn_zarr})"
+        root_grp.attrs["python_version"] = f"{sys.version}"
 
+    print("Start actual calculation")
+    mean = calculate_mean(ds_classifications_input.mask)
+    count[:, :, :, :] = mean.values
+    times[:] = mean.time.values
 
-print("Start actual calculation")
-for d, date in enumerate(tqdm.tqdm(ds_classifications_input.time)):
-    day_sel = ds_classifications_input.sel(time=date)
-
-    count[d, :, :, :] = (
-        calculate_mean(day_sel.mask.fillna(0).astype(int)).compute().astype(float)
-    )
-    times[d] = date.values.astype("<M8[ns]")
-
-zarr.consolidate_metadata(store)
+    zarr.consolidate_metadata(store)
