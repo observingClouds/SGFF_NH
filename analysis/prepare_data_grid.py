4d24510cb336b264f2ee206e1fad15f65a010646
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-03-22 14:11:19 -0700

    consideration of satellite footprint and high clouds
    - add field `domain_coverage` as percentage of 2.5x2.5 grid being
      covered by usable satellite imagery and thus good for pattern
      retrieval
    - `counts` are corrected by available data if new `filter`
      option is set to True
    - `counts` are given as percentage of gridbox.
    
    fixes #4

--- analysis/prepare_data_grid.py
+++ analysis/prepare_data_grid.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import dask
+import dask.array
 import numpy as np
 import tqdm
 import xarray as xr
@@ -16,6 +17,8 @@ fn_zarr = "./data/SGFF/level1/TB/IR_TropicalBelt.zarr"
 output_file = (
     "./data/SGFF/level2/TB/Daily_2.5x2.5_MODIS-IR_TropicalBelt_SGFF_2001-2015.zarr"
 )
+filter = True
+BT_THRESHOLD = 240
 
 grid_spacing = 2.5
 lat_bins = np.arange(-16.25, 16.25 + grid_spacing, grid_spacing)
@@ -39,6 +42,19 @@ def calculate_mean(da):
     return lat_lon_binned
 
 
+def count_cells(da):
+    """
+    Count number of data points in binned grid cells.
+    """
+    lat_cells = da.groupby_bins("latitude", lat_bins, labels=lat_center).count(
+        dim="latitude"
+    )
+    lon_cells = lat_cells.groupby_bins("longitude", lon_bins, labels=lon_center).sum(
+        dim="longitude"
+    )
+    return lon_cells
+
+
 if __name__ == "__main__":
     ds_classifications_input = xr.open_zarr(fn_zarr)
 
@@ -48,6 +64,10 @@ if __name__ == "__main__":
     nb_lons = len(lon_center)
     nb_lats = len(lat_center)
     nb_patterns = len(ds_classifications_input.pattern)
+
+    pixel_per_gridcell = count_cells(
+        ds_classifications_input.mask.isel(time=0, pattern=0)
+    )
 
     if os.path.exists(output_file) and not overwrite:
         print("File already exists.")
@@ -59,6 +79,15 @@ if __name__ == "__main__":
         print("File will be newly created.")
         store = zarr.DirectoryStore(output_file)
         root_grp = zarr.group(store, overwrite=True)
+        if filter:
+            domain_covered = root_grp.create_dataset(
+                "domain_covered",
+                shape=(nb_times, nb_lons, nb_lats),
+                chunks=(1, nb_lons, nb_lats),
+                dtype=float,
+                compressor=zarr.Zlib(level=1),
+                fill_value=-1,
+            )
         count = root_grp.create_dataset(
             "counts",
             shape=(nb_times, nb_lons, nb_lats, nb_patterns),
@@ -108,7 +137,16 @@ if __name__ == "__main__":
         count.attrs["_ARRAY_DIMENSIONS"] = ("time", "longitude", "latitude", "class")
         count.attrs[
             "description"
-        ] = "number of classifications of particular pattern within 2.5 deg x 2.5 deg"
+        ] = "percentage of classifications of particular pattern within 2.5 deg x 2.5 deg"
+        if filter:
+            domain_covered.attrs["_ARRAY_DIMENSIONS"] = (
+                "time",
+                "longitude",
+                "latitude",
+            )
+            domain_covered.attrs[
+                "description"
+            ] = f"percentage of 2.5 deg x 2.5 deg domain covered by satellite footprint and not covered by high clouds (BT < {BT_THRESHOLD}K)"
         lons.attrs["_ARRAY_DIMENSIONS"] = "longitude"
         lons.attrs["standard_name"] = "longitude"
         lons.attrs["units"] = "degree_east"
@@ -136,11 +174,39 @@ if __name__ == "__main__":
 
     print("Start actual calculation")
     slice_size = 1
-    mean = calculate_mean(ds_classifications_input.mask)
+    if filter:
+        # Pixel outside footprint or covered by high clouds
+        retrieval_mask = ds_classifications_input.sat_mask & (
+            ds_classifications_input.sat_bt < BT_THRESHOLD
+        )
+        # Pattern detected and covered by satellite footprint
+        mask = ds_classifications_input.mask & retrieval_mask
+    else:
+        mask = ds_classifications_input.mask
+    mean = calculate_mean(mask)
+
+    if filter:
+        retrieval_sum = calculate_mean(retrieval_mask)
+        mean = (
+            dask.array.true_divide(
+                mean,
+                retrieval_sum,
+                out=dask.array.zeros_like(mean),
+                where=(retrieval_sum > 0).expand_dims("pattern", axis=-1),
+            )
+            * 100
+        )
+        retrieval_perc = retrieval_sum / pixel_per_gridcell * 100
+    else:
+        mean = mean / pixel_per_gridcell
     for d in tqdm.tqdm(range(len(mean.time))):
         count[d : d + slice_size, :, :, :] = mean.isel(
             time=slice(d, d + slice_size)
         ).values
+        if filter:
+            domain_covered[d : d + slice_size, :, :] = retrieval_perc.isel(
+                time=slice(d, d + slice_size)
+            ).values
         times[d : d + slice_size] = mean.isel(time=slice(d, d + slice_size)).time.values
         d += slice_size
 
