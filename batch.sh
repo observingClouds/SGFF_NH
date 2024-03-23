e8d5393e9e9980f4b7a3db803e06968ab0f1185b
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-03-22 17:42:06 -0700

    freeze stages to prevent reproducing expensive computes where output has been deleted

--- batch.sh
+++ batch.sh
@@ -11,8 +11,7 @@ source activate /home/disk/olympus/schulz/miniforge3/envs/schulz_et_al_2023
 cd /home/disk/olympus/schulz/SGFF_NH
 
 # Run your script or command here
-dvc repro -s -f mask_classifications
-dvc repro -s -f grid_mask_TB
+dvc repro grid_mask_TB
 
 # Send an email notification when the job is done
 echo "Your job is done. Please check the output." | mail -s "Job Completed" haschulz@uw.edu
